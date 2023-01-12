57b58b424d4b9d34b5a7b17571ae897721dec3e5
TIME <opout777@yandex.ru>
2023-01-12 13:18:29 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,14 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+        var mass1 = 78.2;
+        System.out.println(mass1 + "кг. Вес первого боксера");
+        var mass2 = 82.7;
+        System.out.println(mass2 + "кг. Вес второго боксера");
+        var totalWeight = mass1 + mass2;
+        System.out.println(totalWeight + "кг. Вес двух бойцов");
+        var odds = mass2 - mass1;
+        System.out.println(odds + "кг. Разница между весами бойцов");
     }
 }
